40a1df30075689386d72d4be0633332cc82a0eac
Mauro Alderete <131892094+MauroAldereteAndroid@users.noreply.github.com>
2023-08-17 22:22:05 -0300

    [Feature] Logic ui buttons

--- app/src/main/java/com/maurodev/chronometer/ui/principal/MainActivity.kt
+++ app/src/main/java/com/maurodev/chronometer/ui/principal/MainActivity.kt
@@ -1,26 +1,28 @@
 package com.maurodev.chronometer.ui.principal
 
+import android.content.Context
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.maurodev.chronometer.R
+import com.maurodev.chronometer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    // TODO hacer binding
-    lateinit var buttonPlay: Button
-    lateinit var buttonStop: Button
-    lateinit var buttonPause: Button
-    lateinit var recyclerView: RecyclerView
-
+    private lateinit var _binding: ActivityMainBinding
+    private var flagButtonPlay = false // Este valor debe vivir en el vm ya que no conserva el estado.
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
 
-        recyclerView = findViewById(R.id.rv_time_cicles)
+        setupRecyclerView(_binding.root.context)
+        behaviorButtons()
+    }
+
+    private fun setupRecyclerView(context: Context) {
         val adapter = RecyclerViewAdapter(
             mutableListOf(
                 "1. 00 : 00 : 01 : 123",
@@ -33,7 +35,32 @@ class MainActivity : AppCompatActivity() {
                 "4. 00 : 33 : 01 : 123"
             )
         )
-        recyclerView.layoutManager = LinearLayoutManager(baseContext)
-        recyclerView.adapter = adapter
+        _binding.rvTimeCicles.layoutManager = LinearLayoutManager(context)
+        _binding.rvTimeCicles.adapter = adapter
+    }
+
+    private fun behaviorButtons() {
+        _binding.bPlay.setOnClickListener {
+            flagButtonPlay = true
+            behaviorButtonPlay()
+        }
+
+        _binding.bPause.setOnClickListener {
+            flagButtonPlay = false
+            behaviorButtonPlay()
+        }
+
+        _binding.bStop.setOnClickListener {
+            flagButtonPlay = false
+            behaviorButtonPlay()
+        }
+    }
+
+    private fun behaviorButtonPlay() {
+        _binding.bPlay.text = if (flagButtonPlay) LOOP else PLAY
+    }
+    companion object {
+        const val LOOP = "Loop"
+        const val PLAY = "Play"
     }
 }
